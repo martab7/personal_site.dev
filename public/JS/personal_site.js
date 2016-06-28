663bd82d8a05d6887c5db0a15d39a560da42d47a
Anthony <martab7.am@gmail.com>
2016-06-28 14:34:23 -0500

    Updates files

--- public/JS/personal_site.js
+++ public/JS/personal_site.js
@@ -1,13 +1,18 @@
 $('.left').hover(
   function () {
-    console.log('hi')
     $('.site-wrapper-inner').css('background-image',
     '-webkit-gradient(linear, left bottom, left top, color-stop(0%, rgba(0, 0, 0, 0)), color-stop(78%, rgba(0, 0, 0, 0)), color-stop(100%, rgba(0, 0, 0, 0.65))), url(pictures/left-mask.png), url(pictures/background_updated.png)'
   )
+  setTimeout(function() {
+    $('.left').append('<h1>Back-End</h1>');
+  }, 1000)
+  $('#left-brain-svg').removeAttr('hidden')
   }, function () {
     $('.site-wrapper-inner').css('background-image',
     '-webkit-gradient(linear, left bottom, left top, color-stop(0%, rgba(0, 0, 0, 0)), color-stop(78%, rgba(0, 0, 0, 0)), color-stop(100%, rgba(0, 0, 0, 0.65))), url(pictures/background_updated.png)'
 )
+$('.left').find( "h1:last" ).remove();
+$('#left-brain-svg').prop('hidden', true);
 }
 );
 
@@ -17,10 +22,14 @@ $('.right').hover(
     console.log('hi')
     $('.site-wrapper-inner').css('background-image',
     '-webkit-gradient(linear, left bottom, left top, color-stop(0%, rgba(0, 0, 0, 0)), color-stop(78%, rgba(0, 0, 0, 0)), color-stop(100%, rgba(0, 0, 0, 0.65))), url(pictures/right-mask.png), url(pictures/background_updated.png)'
-  )
+    )
+    setTimeout(function() {
+      $('.right').append('<h1>Front-End</h1>');
+    }, 1000)
   }, function () {
     $('.site-wrapper-inner').css('background-image',
     '-webkit-gradient(linear, left bottom, left top, color-stop(0%, rgba(0, 0, 0, 0)), color-stop(78%, rgba(0, 0, 0, 0)), color-stop(100%, rgba(0, 0, 0, 0.65))), url(pictures/background_updated.png)'
-)
+    )
+    $('.right').find( "h1:last" ).remove()
 }
 );
